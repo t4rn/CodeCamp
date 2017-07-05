a37bc10f62e606e9b7877acc5aa1738df76de2d5
t4rn <kwitkowski.it@gmail.com>
2017-07-05 16:03:16 +0200

    Getting Started

---
+++ src/CodeCamp/Controllers/CampsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeCamp.Controllers
+{
+    [Route("api/[controller]")]
+    public class CampsController : Controller
+    {
+        [HttpGet("")]
+        public IActionResult Get()
+        {
+            return Ok(new { Name = "Kris", Color = "Blue" });
+        }
+    }
+}
